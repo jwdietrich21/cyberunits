e0f835755f2af24c809bb36cfc7de09d865a2afa
jwdietrich <jwdietrich@aba1714c-8d09-4249-880c-97102217fbc3>
2015-06-15 17:44:48 +0000

    Adding a dead time element (PT0).

--- bricks/bricks.pas
+++ bricks/bricks.pas
@@ -26,7 +26,9 @@ unit bricks;
 { but WITHOUT ANY WARRANTY; without even the implied warranty of }
 { MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. }
 
-{$mode objfpc}{$H+}
+{$mode objfpc}
+{$H+}
+{$ASSERTIONS ON}
 
 interface
 
@@ -34,7 +36,6 @@ uses
   Classes, SysUtils, StrUtils, Math;
 
 const
-
   kError101 = 'Runtime error: Negative parameter(s)';
   kError102 = 'Runtime error: Parameter(s) out of range';
   kError103 = 'Runtime error: min > max';
@@ -68,9 +69,29 @@ type
     property simOutput: real read GetOutput;
   end;
 
+  { TPT0 }
+
+  TPT0 = class(TBlock)
+  {dead-time element, improved from Neuber 1989}
+  protected
+    procedure simulate; override;
+    function GetQueueLength: integer;
+    procedure SetQueueLength(AValue: integer);
+    function GetOutput: real;
+  public
+    input: real;
+    xt: array of real;
+    constructor Create;
+    destructor Destroy; override;
+    property output: real read Foutput;
+    property nt: integer read GetQueueLength write SetQueueLength;
+    property simOutput: real read GetOutput;
+  end;
+
   { TPT1 }
 
   TPT1 = class(TBlock)
+  {First order delay element, changed from Neuber 1989}
   protected
     procedure simulate; override;
     function GetOutput: real;
@@ -84,12 +105,57 @@ type
 
 implementation
 
+{ TPT0 }
+
+function TPT0.GetQueueLength: integer;
+begin
+  result := Length(xt)
+end;
+
+procedure TPT0.SetQueueLength(AValue: integer);
+begin
+  SetLength(xt, AValue);
+end;
+
+procedure TPT0.simulate;
+var
+  i: integer;
+begin
+  assert(nt >= 0, kError101);
+  if nt = 0 then
+    fOutput := input
+  else
+  begin
+    for i := nt - 1 downto 1 do
+      xt[i] := xt[i - 1];
+    fOutput := xt[nt - 1];
+    xt[0] := input;
+  end;
+end;
+
+function TPT0.GetOutput: real;
+begin
+  simulate;
+  result := fOutput;
+end;
+
+constructor TPT0.Create;
+begin
+  inherited Create;
+  fOutput := 0;
+end;
+
+destructor TPT0.Destroy;
+begin
+  inherited Destroy;
+end;
+
 { TPT1 }
 
 function TPT1.GetOutput: real;
 begin
   simulate;
-  result := Foutput;
+  result := fOutput;
 end;
 
 procedure TPT1.simulate;
@@ -98,15 +164,15 @@ var
 begin
   assert((G >= 0) and (t1 >=0), kError101);
   f := exp(-delta / t1);
-  Foutput := f * x0 + G * (1 - f) * input;
-  x0 := Foutput;
+  fOutput := f * x0 + G * (1 - f) * input;
+  x0 := fOutput;
 end;
 
 constructor TPT1.Create;
 begin
   inherited Create;
   x0 := 0;
-  foutput := 0;
+  fOutput := 0;
 end;
 
 destructor TPT1.Destroy;
@@ -119,19 +185,19 @@ end;
 function TP.GetOutput: real;
 begin
   simulate;
-  result := Foutput;
+  result := fOutput;
 end;
 
 procedure TP.simulate;
 begin
   assert(G >= 0, kError101);
-  Foutput := input * G;
+  fOutput := input * G;
 end;
 
 constructor TP.Create;
 begin
   inherited Create;
-  foutput := 0;
+  fOutput := 0;
 end;
 
 destructor TP.Destroy;
@@ -147,4 +213,8 @@ begin
 end;
 
 end.
+
+{References:  }
+{1. Neuber, H., "Simulation von Regelkreisen auf Personal Computern  }
+{   in Pascal und Fortran 77", IWT, Vaterstetten 1989  }
 
