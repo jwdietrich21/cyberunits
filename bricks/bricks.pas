db1daf963c0fbde36816f07f7b6002927975cf4b
jwdietrich <jwdietrich@aba1714c-8d09-4249-880c-97102217fbc3>
2015-06-15 12:19:45 +0000

    Implementing a PT1 block.

--- bricks/bricks.pas
+++ bricks/bricks.pas
@@ -33,6 +33,13 @@ interface
 uses
   Classes, SysUtils, StrUtils, Math;
 
+const
+
+  kError101 = 'Runtime error: Negative parameter(s)';
+  kError102 = 'Runtime error: Parameter(s) out of range';
+  kError103 = 'Runtime error: min > max';
+  kError104 = 'Runtime error: max = 0';
+
 type
 
   { TBlock }
@@ -40,9 +47,9 @@ type
   TBlock = class
   protected
     Foutput: real;
+    procedure simulate; virtual; abstract;
   public
     name: string;
-    procedure simulate; virtual; abstract;
     destructor Destroy; override;
     property output: real read Foutput;
   end;
@@ -51,21 +58,63 @@ type
 
   TP = class(TBlock)
   protected
+    procedure simulate; override;
     function GetOutput: real;
   public
     input, G: real;
-    procedure simulate; override;
     constructor Create;
     destructor Destroy; override;
-    property output: real read GetOutput;
+    property output: real read Foutput;
+    property simOutput: real read GetOutput;
+  end;
+
+  { TPT1 }
+
+  TPT1 = class(TBlock)
+  protected
+    procedure simulate; override;
+    function GetOutput: real;
+  public
+    input, G, t1, x0, delta: real;
+    constructor Create;
+    destructor Destroy; override;
+    property output: real read Foutput;
+    property simOutput: real read GetOutput;
   end;
 
 implementation
 
-destructor TBlock.Destroy;
+{ TPT1 }
+
+function TPT1.GetOutput: real;
+begin
+  simulate;
+  result := Foutput;
+end;
+
+procedure TPT1.simulate;
+var
+  f: real;
+begin
+  assert((G >= 0) and (t1 >=0), kError101);
+  f := exp(-delta / t1);
+  Foutput := f * x0 + G * (1 - f) * input;
+  x0 := Foutput;
+end;
+
+constructor TPT1.Create;
+begin
+  inherited Create;
+  x0 := 0;
+  foutput := 0;
+end;
+
+destructor TPT1.Destroy;
 begin
   inherited Destroy;
 end;
+
+  { TP }
 
 function TP.GetOutput: real;
 begin
@@ -75,15 +124,24 @@ end;
 
 procedure TP.simulate;
 begin
+  assert(G >= 0, kError101);
   Foutput := input * G;
 end;
 
 constructor TP.Create;
 begin
   inherited Create;
+  foutput := 0;
 end;
 
 destructor TP.Destroy;
+begin
+  inherited Destroy;
+end;
+
+  { TBlock }
+
+destructor TBlock.Destroy;
 begin
   inherited Destroy;
 end;
