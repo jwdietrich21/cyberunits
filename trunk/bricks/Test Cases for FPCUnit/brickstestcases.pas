b441d477a4b6d0cc6d32723169e91e21292e588a
jwdietrich <jwdietrich@aba1714c-8d09-4249-880c-97102217fbc3>
2015-06-15 12:19:45 +0000

    Implementing a PT1 block.

--- trunk/bricks/Test Cases for FPCUnit/brickstestcases.pas
+++ trunk/bricks/Test Cases for FPCUnit/brickstestcases.pas
@@ -35,12 +35,77 @@ uses
 
 type
 
+  { TControlTestCases }
+
+  TControlTestCases = class(TTestCase)
+  published
+    procedure PositiveCheck;
+  end;
+
+{ TPTestCases }
+
   TPTestCases = class(TTestCase)
   published
     procedure Test1;
   end;
 
+{ TPT1TestCases }
+
+  TPT1TestCases = class(TTestCase)
+  published
+    procedure Test1;
+    procedure Test2;
+  end;
+
 implementation
+
+{ TControlTestCases }
+
+procedure TControlTestCases.PositiveCheck;
+{ Positive check, should always succeed }
+begin
+  AssertNull('This test is bound to succeed', nil);
+end;
+
+{ TPT1TestCases }
+
+procedure TPT1TestCases.Test1;
+var
+  testBrick: TPT1;
+  temp: real;
+  i: integer;
+begin
+  testBrick := TPT1.Create;
+  testBrick.G := 5;
+  testBrick.delta := 10;
+  testBrick.t1 := 15;
+  testBrick.input := 2;
+  AssertEquals(0, TestBrick.output);
+  for i := 1 to 10000 do
+    temp := TestBrick.simOutput;
+  AssertEquals(testBrick.G * testBrick.input, TestBrick.output);
+  testBrick.Destroy;
+end;
+
+procedure TPT1TestCases.Test2;
+var
+  testBrick: TPT1;
+  temp: real;
+  i: integer;
+begin
+  testBrick := TPT1.Create;
+  testBrick.G := 5;
+  testBrick.delta := 1;
+  testBrick.t1 := 15e6;
+  testBrick.input := 2;
+  AssertEquals(0, TestBrick.output);
+  for i := 1 to 100000 do
+    temp := TestBrick.simOutput;
+  AssertTrue(TestBrick.output < 0.07);
+  testBrick.Destroy;
+end;
+
+{ TPTestCases }
 
 procedure TPTestCases.Test1;
 var
@@ -49,12 +114,14 @@ begin
   testBrick := TP.Create;
   testBrick.G := 5;
   testBrick.input := 2;
-  AssertEquals(10, testBrick.output);
+  AssertEquals(10, testBrick.simOutput);
   testBrick.Destroy;
 end;
 
 initialization
 
+  RegisterTest(TControlTestCases);
   RegisterTest(TPTestCases);
+  RegisterTest(TPT1TestCases);
 end.
 
