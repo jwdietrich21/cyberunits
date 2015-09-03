5a3a94b74493227768165fac2b07bed443b1f539
jwdietrich <jwdietrich@aba1714c-8d09-4249-880c-97102217fbc3>
2015-09-03 12:40:39 +0000

    Demo program for 0th linear feedback control partly rewritten to make use of SystemsDiagram unit.

--- trunk/bricks/Example_Programs/Linear 0th order Feedback Control/ips.pas
+++ trunk/bricks/Example_Programs/Linear 0th order Feedback Control/ips.pas
@@ -32,7 +32,8 @@ unit IPS;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  lclintf, SystemsDiagram;
 
 type
 
@@ -40,6 +41,8 @@ type
 
   TIPSForm = class(TForm)
     IPSImage: TImage;
+    procedure DrawIPS(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { private declarations }
   public
@@ -52,6 +55,142 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TIPSForm }
+
+procedure TIPSForm.DrawIPS(Sender: TObject);
+var
+  BlockDiagram: TBlockDiagram;
+  IPSBitmap: TBitmap;
+  Controller, LoadInjection, G1, G2, xt, zt: TIPSClass;
+  cx, ce, cy, cys, cyr, cz: TConnectionClass;
+begin
+  BlockDiagram := TBlockDiagram.Create;
+  IPSBitmap := TBitmap.Create;
+  try
+    IPSBitmap.Height := IPSImage.Height;
+    IPSBitmap.Width := IPSImage.Width;
+    IPSBitmap.Canvas.Brush.Color := clWhite;
+    IPSBitmap.Canvas.Pen.Color := clWhite;
+    IPSBitmap.Canvas.Rectangle(0, 0, IPSBitmap.Width, IPSBitmap.Height);
+    IPSBitmap.Canvas.Pen.Color := clBlack;
+    BlockDiagram.canvas := IPSBitmap.Canvas;
+
+    xt := TTerminalClass.Create;
+    xt.blockDiagram := BlockDiagram;
+    BlockDiagram.firstIPSObject := xt;
+    SetRect(xt.boundsRect, 31, 20, 51, 40);
+    xt.title := 'x(t)';
+    TTerminalClass(xt).TextMargin := 5;
+    TTerminalClass(xt).TextPosition := rightmiddle;
+    xt.Draw;
+
+    Controller := TSigmaClass.Create;
+    Controller.blockDiagram := BlockDiagram;
+    SetRect(Controller.boundsRect, 20, 60, 60, 100);
+    TInvertableClass(Controller).invertedSegments := [bottomSegment];
+    Controller.Draw;
+    xt.Next := Controller;
+
+    cx := TConnectionClass.Create;
+    cx.blockDiagram := BlockDiagram;
+    cx.sourceObject := xt;
+    cx.sourceAnchor := bottommiddle;
+    cx.drainObject := Controller;
+    cx.drainAnchor := topmiddle;
+    cx.Draw;
+    Controller.Next := cx;
+
+    G1 := TPClass.Create;
+    G1.blockDiagram := BlockDiagram;
+    SetRect(G1.boundsRect, 150, 62, 210, 98);
+    G1.title := 'G1';
+    G1.font.Style := [fsItalic];
+    G1.Draw;
+    cx.Next := G1;
+
+    ce := TConnectionClass.Create;
+    ce.blockDiagram := BlockDiagram;
+    ce.sourceObject := Controller;
+    ce.sourceAnchor := rightmiddle;
+    ce.drainObject := G1;
+    ce.drainAnchor := leftmiddle;
+    ce.Draw;
+    G1.Next := ce;
+
+    LoadInjection := TSigmaClass.Create;
+    LoadInjection.blockDiagram := BlockDiagram;
+    SetRect(LoadInjection.boundsRect, 290, 120, 310, 160);
+    LoadInjection.Draw;
+    ce.Next := LoadInjection;
+
+    cys := TConnectionClass.Create;
+    cys.blockDiagram := BlockDiagram;
+    cys.sourceObject := G1;
+    cys.sourceAnchor := rightmiddle;
+    cys.drainObject := LoadInjection;
+    cys.drainAnchor := topmiddle;
+    cys.chirality := cright;
+    cys.Draw;
+    LoadInjection.Next := cys;
+
+    G2 := TPClass.Create;
+    G2.blockDiagram := BlockDiagram;
+    SetRect(G2.boundsRect, 150, 190, 210, 226);
+    G2.title := 'G2';
+    G2.font.Style := [fsItalic];
+    G2.Draw;
+    cys.Next := G2;
+
+    cy := TConnectionClass.Create;
+    cy.blockDiagram := BlockDiagram;
+    cy.sourceObject := LoadInjection;
+    cy.sourceAnchor := bottommiddle;
+    cy.drainObject := G2;
+    cy.drainAnchor := rightmiddle;
+    cy.chirality := cright;
+    cy.Draw;
+    G2.Next := cy;
+
+    cyr := TConnectionClass.Create;
+    cyr.blockDiagram := BlockDiagram;
+    cyr.sourceObject := G2;
+    cyr.sourceAnchor := leftmiddle;
+    cyr.drainObject := Controller;
+    cyr.drainAnchor := bottommiddle;
+    cyr.chirality := cright;
+    cyr.Draw;
+    cy.Next := cyr;
+
+    zt := TTerminalClass.Create;
+    zt.blockDiagram := BlockDiagram;
+    SetRect(zt.boundsRect, 340, 121, 380, 161);
+    zt.title := 'z(t)';
+    TTerminalClass(zt).TextMargin := 5;
+    TTerminalClass(zt).TextPosition := rightmiddle;
+    zt.Draw;
+    cyr.Next := zt;
+
+    cz := TConnectionClass.Create;
+    cz.blockDiagram := BlockDiagram;
+    cz.sourceObject := zt;
+    cz.sourceAnchor := leftmiddle;
+    cz.drainObject := LoadInjection;
+    cz.drainAnchor := rightmiddle;
+    cz.Draw;
+    zt.Next := cz;
+
+    IPSImage.Canvas.Draw(0, 0, IPSBitmap);
+  finally
+    IPSBitmap.Free;
+    BlockDiagram.Destroy;
+  end;
+end;
+
+procedure TIPSForm.FormCreate(Sender: TObject);
+begin
+  DrawIPS(Sender);
+end;
 
 end.
 
