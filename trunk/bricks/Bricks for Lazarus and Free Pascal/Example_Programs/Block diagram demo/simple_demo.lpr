d2004b2cc6ab78ee6cfe4a868c60b2562bdefad6
jwdietrich <jwdietrich@aba1714c-8d09-4249-880c-97102217fbc3>
2015-09-08 19:41:22 +0000

    Preparing versions for S and MATLAB.

---
+++ trunk/bricks/Bricks for Lazarus and Free Pascal/Example_Programs/Block diagram demo/simple_demo.lpr
@@ -0,0 +1,47 @@
+program simple_demo;
+
+{ CyberUnits }
+
+{ Object Pascal units for computational cybernetics }
+
+{ Demo of canvas using SystemsDiagram }
+{ Main project file }
+
+{ Version 1.1.0 (Corvus) }
+
+{ (c) Johannes W. Dietrich, 1994 - 2015 }
+{ (c) Ludwig Maximilian University of Munich 1996 - 2002 }
+{ (c) University of Ulm Hospitals 2002-2004 }
+{ (c) Ruhr University of Bochum 2005 - 2015 }
+
+{ Source code released under the BSD License }
+
+{ See the file "license.txt", included in this distribution, }
+{ for details about the copyright. }
+{ Current versions and additional information are available from }
+{ http://cyberunits.sf.net }
+
+{ This program is distributed in the hope that it will be useful, }
+{ but WITHOUT ANY WARRANTY; without even the implied warranty of }
+{ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. }
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, gui, bricks, systemsdiagram, lifeblocks
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  Application.Title:='Simple Demo for SystemsDiagram';
+  RequireDerivedFormResource := True;
+  Application.Initialize;
+  Application.CreateForm(TDemoForm, DemoForm);
+  Application.Run;
+end.
+
---
+++ trunk/bricks/Bricks for Lazarus and Free Pascal/Example_Programs/Linear 0th order Feedback Control/plot.pas
@@ -0,0 +1,84 @@
+unit Plot;
+
+{ CyberUnits }
+
+{ Object Pascal units for computational cybernetics }
+
+{ Demo of a simple simulator for a linear 0th order feedback system }
+{ Plot unit }
+
+{ Version 1.1.0 (Corvus) }
+
+{ (c) Johannes W. Dietrich, 1994 - 2015 }
+{ (c) Ludwig Maximilian University of Munich 1995 - 2002 }
+{ (c) University of Ulm Hospitals 2002-2004 }
+{ (c) Ruhr University of Bochum 2005 - 2015 }
+
+{ Standard blocks for systems modelling and simulation }
+
+{ Source code released under the BSD License }
+
+{ See the file "license.txt", included in this distribution, }
+{ for details about the copyright. }
+{ Current versions and additional information are available from }
+{ http://cyberunits.sf.net }
+
+{ This program is distributed in the hope that it will be useful, }
+{ but WITHOUT ANY WARRANTY; without even the implied warranty of }
+{ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. }
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, TAGraph, TASeries, TALegendPanel, Forms,
+  Controls, Graphics, Dialogs, SimulationEngine;
+
+type
+
+  { TPlotForm }
+
+  TPlotForm = class(TForm)
+    Chart1: TChart;
+    ChartLegendPanel1: TChartLegendPanel;
+    zSeries: TLineSeries;
+    ySeries: TLineSeries;
+    eSeries: TLineSeries;
+    ysSeries: TLineSeries;
+    yrSeries: TLineSeries;
+    xSeries: TLineSeries;
+  private
+    { private declarations }
+  public
+    { public declarations }
+    procedure ShowPlot;
+  end;
+
+var
+  PlotForm: TPlotForm;
+
+implementation
+
+{$R *.lfm}
+
+{ TPlotForm }
+
+procedure TPlotForm.ShowPlot;
+var
+  i: integer;
+begin
+  Chart1.AxisList.Axes[1].Range.Max := gValues.size - 1;
+  for i := 0 to gValues.size - 1 do
+  begin
+    xSeries.AddXY(i, gValues.x[i]);
+    zSeries.AddXY(i, gValues.z[i]);
+    ySeries.AddXY(i, gValues.y[i]);
+    eSeries.AddXY(i, gValues.e[i]);
+    ysSeries.AddXY(i, gValues.ys[i]);
+    yrSeries.AddXY(i, gValues.yr[i]);
+  end;
+end;
+
+end.
+
