944ae49fa49a6b19e0f9904700aa91fe67735aa2
jwdietrich <jwdietrich@aba1714c-8d09-4249-880c-97102217fbc3>
2020-03-27 14:23:28 +0000

    Minor optimisations

--- bricks/Example_Programs/Linear 1st order Feedback Control/simulationengine.pas
+++ bricks/Example_Programs/Linear 1st order Feedback Control/simulationengine.pas
@@ -7,12 +7,12 @@ unit SimulationEngine;
 { Demo of a simple simulator for a linear 1st order feedback system }
 { Simulation Engine }
 
-{ Version 1.0.1 (Corvus) }
+{ Version 1.1.0 (Dendron) }
 
-{ (c) Johannes W. Dietrich, 1994 - 2019 }
+{ (c) Johannes W. Dietrich, 1994 - 2020 }
 { (c) Ludwig Maximilian University of Munich 1995 - 2002 }
-{ (c) University of Ulm Hospitals 2002-2004 }
-{ (c) Ruhr University of Bochum 2005 - 2019 }
+{ (c) University of Ulm Hospitals 2002 - 2004 }
+{ (c) Ruhr University of Bochum 2005 - 2020 }
 
 { Standard blocks for systems modelling and simulation }
 
